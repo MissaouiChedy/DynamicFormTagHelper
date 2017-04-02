a45c2a7777adf24d990934ecac1d943f3dded4b3
Chedy Missaoui <missaoui.chedy@gmail.com>
2017-04-02 10:20:51 +0100

    Created a datetime picker widget template

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,10 @@
 // Write your Javascript code.
+$(function () {
+    $('.datetimepicker').each(function (index, elem) {
+        $(elem).datetimepicker({
+            format: "YYYY-MM-DD[T]HH:mm:ss.SSS[Z]",
+            showClear: true,
+            showTodayButton: true
+        });
+    });
+});
