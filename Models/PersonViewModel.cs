cf8c6043fac515339350e624a6d0d1d85c864495
Chedy Missaoui <missaoui.chedy@gmail.com>
2017-04-10 12:42:26 +0100

    Added Required attributes on the choices selection fields

--- Models/PersonViewModel.cs
+++ Models/PersonViewModel.cs
@@ -23,7 +23,7 @@ namespace DynamicFormTagHelper.Models
         [Display(Name = "Person's full name")]
         public string Name { get; set; }
         
-        private Sex _gender { get; set; }
+        public Sex _gender { get; set; }
 
         [Required]
         [Display(Name= "Person's Gender")]
@@ -39,6 +39,34 @@ namespace DynamicFormTagHelper.Models
                 _gender = (Sex) value;
             }
         }
+        [DataType(DataType.Date)]
+        [Display(Name = "Birth date")]
+        public DateTime? BirthDate { get; set; }
+
+        [Display(Name = "BasketBall Player?")]
+        public bool IsBasketBallPlayer { get; set; } = false;
+
+        [Required]
+        [Display(Name = "Item")]
+        [ItemsSource(ItemsProperty = nameof(Items))]
+        public string SelectedItem { get; set; }
+
+        [Required]
+        [Display(Name = "Multiple Items")]
+        [ItemsSource(ItemsProperty = nameof(Items))]
+        public List<string> SelectedItems { get; set; }
+
+        [Required]
+        [Display(Name = "Single Item")]
+        [ItemsSource(ItemsProperty = nameof(Items), ChoicesType = ChoicesTypes.RADIO)]
+        public string SingleItem { get; set; }
+        
+        
+
+        [Display(Name = "Person's Dog")]
+        public DogViewModel OwnedDog { get; set; } = new DogViewModel();
+
+        public string IsBasketBallPlayerVM => IsBasketBallPlayer ? "Yes" : "No";
 
         public List<SelectListItem> Items => new List<SelectListItem>()
         {
@@ -47,31 +75,6 @@ namespace DynamicFormTagHelper.Models
             new SelectListItem {Value = "2", Text="Item 2"},
             new SelectListItem {Value = "3", Text="Item 3"},
         };
-
-        [Display(Name = "Item")]
-        [ItemsSource(ItemsProperty = nameof(Items))]
-        public string SelectedItem { get; set; }
-        
-        [Display(Name = "Multiple Items")]
-        [ItemsSource(ItemsProperty = nameof(Items))]
-        public List<string> SelectedItems { get; set; }
-
-        [Display(Name = "Single Item")]
-        [ItemsSource(ItemsProperty = nameof(Items), ChoicesType = ChoicesTypes.RADIO)]
-        public string SingleItem { get; set; }
-        
-        [DataType(DataType.Date)]
-        [Display(Name = "Birth date")]
-        public DateTime? BirthDate { get; set; }
-
-        [Display(Name = "BasketBall Player?")]
-        public bool IsBasketBallPlayer { get; set; } = false;
-
-        [Display(Name = "Person's Dog")]
-        public DogViewModel OwnedDog { get; set; } = new DogViewModel();
-
-        public string IsBasketBallPlayerVM => IsBasketBallPlayer ? "Yes" : "No";
-
     }
 
     public class DogViewModel
@@ -94,15 +97,16 @@ namespace DynamicFormTagHelper.Models
     {
         public string Name { get; set; }
 
+        [Required]
+        [Display(Name="Type of the leash")]
+        [ItemsSource(ItemsProperty = nameof(KindsOfLeashes))]
+        public int LeashKind { get; set; }
+
         public List<SelectListItem> KindsOfLeashes => new List<SelectListItem>
         {
             new SelectListItem(){Value = "0", Text = "High end"},
             new SelectListItem(){Value = "1", Text = "Mid tier"},
             new SelectListItem(){Value = "2", Text = "Low end"},
         };
-
-        [Display(Name="Type of the leash")]
-        [ItemsSource(ItemsProperty = nameof(KindsOfLeashes))]
-        public List<int> LeashKind { get; set; }
     }
 }
