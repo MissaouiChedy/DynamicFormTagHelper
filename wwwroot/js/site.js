ed855960a84b935d344b60fba7b53e9e986b044f
Chedy Missaoui <missaoui.chedy@gmail.com>
2017-04-18 11:37:26 +0100

    Corrected Bloodhound dependency Wrote client side code for handling local sourced suggestions

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,10 +1,28 @@
-// Write your Javascript code.
-$(function () {
+$(document).ready(function () {
     $('.datetimepicker').each(function (index, elem) {
         $(elem).datetimepicker({
             format: "YYYY-MM-DD[T]HH:mm:ss.SSS[Z]",
             showClear: true,
             showTodayButton: true
         });
+    });
+
+    $('input.autocomplete').each(function (index, elem) {
+        if ($(elem).data('source-local')) {
+            var suggestionEngine = new Bloodhound({
+                datumTokenizer: Bloodhound.tokenizers.whitespace,
+                queryTokenizer: Bloodhound.tokenizers.whitespace,
+                local: $(elem).data('source-local')
+            });
+            suggestionEngine.initialize();
+            $(elem).typeahead({
+                items: 4,
+                source: suggestionEngine.ttAdapter(),
+                autoSelect: true
+            });
+        }
+        else if ($(elem).data('source-ajax')) {
+
+        }
     });
 });
